d20315b5e17c79501db884802863d5a941833c43
baayso <baayso@outlook.com>
2018-05-09 17:14:52 +0800

    Upload code.

---
+++ src/load_own_images.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# python notebook for Make Your Own Neural Network
+# code for a 3-layer neural network, and code for learning the MNIST dataset
+# (c) Tariq Rashid, 2016
+# license is GPLv2
+
+# helper to load data from PNG image files
+import scipy.misc
+# glob helps select multiple files using patterns
+import glob
+import numpy
+# library for plotting arrays
+import matplotlib.pyplot
+
+# our own image test data set
+our_own_dataset = []
+
+for image_file_name in glob.glob('../my_own_images/2828_my_own_?.png'):
+    print("loading ... ", image_file_name)
+    # use the filename to set the correct label
+    label = int(image_file_name[-5:-4])
+    # load image data from png files into an array
+    img_array = scipy.misc.imread(image_file_name, flatten=True)
+    # reshape from 28x28 to list of 784 values, invert values
+    img_data = 255.0 - img_array.reshape(784)
+    # then scale data to range from 0.01 to 1.0
+    img_data = (img_data / 255.0 * 0.99) + 0.01
+    print(numpy.min(img_data))
+    print(numpy.max(img_data))
+    # append label and image data  to test data set
+    record = numpy.append(label, img_data)
+    print(record)
+    our_own_dataset.append(record)
+    pass
+
+matplotlib.pyplot.imshow(our_own_dataset[3][1:].reshape(28, 28), cmap='Greys', interpolation='None')
+matplotlib.pyplot.show()
+
+print(our_own_dataset[0])
